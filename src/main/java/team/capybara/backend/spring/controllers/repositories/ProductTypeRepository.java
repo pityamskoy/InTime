fd882dac5a722f1d5ae3d557e3b33b238ebab874
Кекишев Андрей Сергеевич <askekishev@mai.education>
2025-10-21 15:05:05 +0300

    fix!: hotfix of ProductTypeRepository

--- src/main/java/team/capybara/backend/spring/controllers/repositories/ProductTypeRepository.java
+++ src/main/java/team/capybara/backend/spring/controllers/repositories/ProductTypeRepository.java
@@ -1,7 +1,9 @@
 package team.capybara.backend.spring.controllers.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import team.capybara.backend.hibernate.ProductType;
 
+@Repository
 public interface ProductTypeRepository extends JpaRepository<ProductType, String> {
 }
