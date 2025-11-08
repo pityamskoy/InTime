b2408ffa6c6c2dae5122cebd012a904fa6aeb65b
Кекишев Андрей Сергеевич <askekishev@mai.education>
2025-11-08 18:41:49 +0300

    fix(ShopService): slightly fix ShopService

--- src/main/java/team/capybara/backend/spring/controllers/services/ShopService.java
+++ src/main/java/team/capybara/backend/spring/controllers/services/ShopService.java
@@ -39,19 +39,22 @@ public final class ShopService {
         return shopMapper.postEntity(shopToCreate);
     }
     public Optional<Double> getShopStarsById(UUID id) {
+        Optional<Shop> shop = shopRepository.findById(id);
 
-        List<Review> rewiews = reviewRepository.findByShop(shopRepository.findById(id).get());
-        Double sumOfStarsValue  = (double) 0;
-        Double colOfReview  = (double) 0;
+        if (shop.isEmpty()) {
+            throw new EntityNotFoundException("Shop not found; id=" + id);
+        }
 
-        for(Review review:rewiews){
+        List<Review> reviews = reviewRepository.findByShop(shop.get());
+        double sumOfStarsValue = 0.0;
+        double colOfReview = 0.0;
+
+        for (Review review:reviews) {
             sumOfStarsValue+=review.getStars();
             colOfReview++;
         }
 
-        Optional<Double> stars = Optional.of(sumOfStarsValue / colOfReview);
-
-        return stars;
+        return Optional.of(sumOfStarsValue / colOfReview);
     }
 
     public ShopDto updateShop(ShopDto shopToUpdate){
