eeeddba8991b10c9d4fc91f3b3d66a2d1d9ab568
Кекишев Андрей Сергеевич <askekishev@mai.education>
2025-11-25 20:42:48 +0300

    fix(services): hotfix of FilteredProductService

--- src/main/java/team/capybara/backend/spring/controllers/services/FilteredProductService.java
+++ src/main/java/team/capybara/backend/spring/controllers/services/FilteredProductService.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
-import team.capybara.backend.spring.controllers.dto.category.CategoryDto;
+import team.capybara.backend.spring.controllers.dto.category.CategoryWithIdDto;
 import team.capybara.backend.spring.controllers.dto.product.ProductWithIdDto;
 import team.capybara.backend.spring.controllers.dto.shop.ShopWithIdDto;
 import team.capybara.backend.spring.controllers.dto.user.UserAuthWithIdDto;
@@ -120,7 +120,7 @@ public final class FilteredProductService {
         List<Product> productsSorted = new ArrayList<>();
 
         for (UUID categoryId : categoriesId) {
-            Optional<CategoryDto> categoryDtoOptional = categoryService.getCategoryById(categoryId);
+            Optional<CategoryWithIdDto> categoryDtoOptional = categoryService.getCategoryById(categoryId);
 
             categoryDtoOptional.ifPresent(categoryDto ->
                     requiresProductTypesId.addAll(categoryDto.productTypesId()));
