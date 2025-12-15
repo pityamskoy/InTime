838e88d7000baeb4fae0fff8d7d3090586dd539f
Кекишев Андрей Сергеевич <askekishev@mai.education>
2025-12-15 15:36:14 +0300

    fix(db connection): fix connection to db from previous merging

--- src/main/java/team/capybara/backend/spring/controllers/controllers/UserController.java
+++ src/main/java/team/capybara/backend/spring/controllers/controllers/UserController.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
+// @CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
 @SuppressWarnings(value = {"unused"})
 public final class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
