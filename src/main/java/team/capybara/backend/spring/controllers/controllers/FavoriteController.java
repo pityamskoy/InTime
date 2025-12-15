5ad3b2dd24f09b2d63399cdc4048dd5488298a25
Leonid Gurianov <leonid.11521@gmail.com>
2025-12-15 17:52:03 +0300

    crossOrigin fix2

--- src/main/java/team/capybara/backend/spring/controllers/controllers/FavoriteController.java
+++ src/main/java/team/capybara/backend/spring/controllers/controllers/FavoriteController.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/favorites")
-@CrossOrigin(origins = {"https://vsrok.bloodstone.boo"}, allowCredentials = "true")
+@CrossOrigin(origins = {"https://vsrok1.bloodstone.boo"}, allowCredentials = "true")
 // @CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
 @SuppressWarnings(value = {"unused"})
 public final class FavoriteController {
--- src/main/java/team/capybara/backend/spring/controllers/controllers/FeedController.java
+++ src/main/java/team/capybara/backend/spring/controllers/controllers/FeedController.java
@@ -18,7 +18,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/feed")
-@CrossOrigin(origins = {"https://vsrok.bloodstone.boo"}, allowCredentials = "true")
+@CrossOrigin(origins = {"https://vsrok1.bloodstone.boo"}, allowCredentials = "true")
 // @CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
 @SuppressWarnings(value = {"unused"})
 public final class FeedController{
